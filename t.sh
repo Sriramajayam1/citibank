86f4ec4b4a4317b965748fe4b3473d452b6a2c17
kayamvasu <kayamvasuachari1@gmail.com>
2023-03-14 14:30:12 +0530

    nothing

---
+++ t.sh
@@ -0,0 +1,11 @@
+echo "enter the text as given below"
+echo "Git is a version control system that allows developers to track changes in their code over time and collaborate with other developers on a project. It was created by Linus Torvalds in 2005 and has since become one of the most widely used version control systems."
+read text
+if [ "$text" = "Git is a version control system that allows developers to track changes in their code over time and collaborate with other developers on a project. It was created by Linus Torvalds in 2005 and has since become one of the most widely used version control systems." ]
+
+then
+	echo "Congratulations!you've passed the test !"
+else 
+	echo "Nidrapora pumka ! fatal error vahindi ,try again ! you have failed to enter the text as above "
+fi
+echo "i've updated this code for nothing!"
---
+++ typing.sh
@@ -0,0 +1,10 @@
+echo "enter The quick brown fox jumps over the lazy dog"
+read a 
+if [ "$a" = "The quick brown fox jumps over the lazy dog" ]
+then
+	echo "you have entered the correct text"
+else
+	echo "wrong input"
+	fi
+echo "test end"
+
